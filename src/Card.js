3c236459f0626701fbd7b55266b1812250b590db
Biggie <biggie@Biggies-MacBook-Pro.local>
2021-03-23 18:00:56 +0100

    first commit

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = (props) => {
+    const { name, email, id, username } = props;
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5 tc'> 
+            <img alt='wide reciever' src={`https://robohash.org/${id}?200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{username}</p>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,58 @@
+export const robots = [
+    {
+        id : 1,
+        name : 'Chase Claypool',
+        username : 'chase',
+        email : 'chase@steelers.com'
+    },
+    {
+        id : 2,
+        name : 'DK Metcalf',
+        username : 'DK',
+        email : 'Metcalfe@seahawks.com'
+    },
+    {
+        id : 3,
+        name : 'JJ Watt',
+        username : 'JJ',
+        email : 'jj@cardinals.com'
+    },
+    {
+        id : 4,
+        name : 'Deandre Hopkins',
+        username : 'D-Hop',
+        email : 'Dhop@cardinals.com'
+    },
+    {
+        id : 5,
+        name : 'Mike Evans',
+        username : 'Mike',
+        email : 'Mike@tampa.com'
+    },
+    {
+        id : 6,
+        name : 'Deshaun Watson',
+        username : 'Dehaun',
+        email : 'deshaun@texans.com'
+    },
+    {
+        id : 7,
+        name : 'Tom Brady',
+        username : 'TB12',
+        email : 'Tb@goat.com'
+    },
+    {
+        id : 8,
+        name : 'Lamar Jackson',
+        username : 'Big Truzz',
+        username : 'Truzz',
+        email : 'lamar@truzz.com'
+    },
+    {
+        id : '9',
+        name : 'Odell Beckham',
+        username : 'OBJ',
+        email : 'Odell@browns.com'
+    },
+
+]
